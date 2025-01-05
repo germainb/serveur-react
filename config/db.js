b3bd1152087d479504446bac4563691d3cea960e
Germain Bataille <g_bataille@hotmail.com>
2025-01-04 23:45:23 -0500

    Show DB3

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log(`MongoDB Connected: ${conn.connection.host} DB: ${conn.connection.db} Collections: ${conn.connection.collections} `);
+        console.log(`MongoDB Connected: ${conn.connection.host} DB: ${conn.connection.db.databaseName} Collections: ${conn.connection.collections.collections} `);
     } catch (err) {
         console.error(`Error: ${err.message}`);
         process.exit(1); // Exit process with failure

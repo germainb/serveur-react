9d0544d57d69e448411de5e36f5b86883db81eb3
Germain Bataille <g_bataille@hotmail.com>
2025-01-06 14:46:54 -0500

    upload folder creation

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -3,7 +3,7 @@ const path = require('path');
 const multer = require('multer');
 
 // Define the upload directory
-const uploadDir = path.join(__dirname, '../..','uploads', 'avatars');
+const uploadDir = path.join(__dirname, '..','uploads', 'avatars');
 
 // Create the directory if it doesn't exist
 if (!fs.existsSync(uploadDir)) {

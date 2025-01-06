13bd87b4f5c80972f80158f36c6dfcb79aa711f5
Germain Bataille <g_bataille@hotmail.com>
2025-01-06 14:27:43 -0500

    upload added

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -3,13 +3,13 @@ const path = require('path');
 const multer = require('multer');
 
 // Define the upload directory
-const uploadDir = path.join(__dirname, 'uploads', 'avatars');
-
+const uploadDir = path.join('..', 'uploads', 'avatars');
+/*
 // Create the directory if it doesn't exist
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
-
+*/
 // Set up multer storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {

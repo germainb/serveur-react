53f998ec59765160d01f59e9a6314ae25dea2a97
Germain Bataille <g_bataille@hotmail.com>
2025-01-06 09:53:14 -0500

    remove databaseName

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log(`MongoDB Connected: ${conn.connection.host} DB: ${conn.connection.db.databaseName}`);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
         console.error(`Error: ${err.message}`);
         process.exit(1); // Exit process with failure

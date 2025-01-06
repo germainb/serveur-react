2f82e0aae97fbf451e690cab6ace7cae35a32fac
Germain Bataille <g_bataille@hotmail.com>
2025-01-06 18:35:36 -0500

    Disabled upload

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
-
+/*
 // Define the upload directory
 const uploadDir = path.join(__dirname, '..','uploads', 'avatars');
-/*
+
 // Create the directory if it doesn't exist
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
-*/
+
 // Set up multer storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -23,5 +23,5 @@ const storage = multer.diskStorage({
 
 // Multer middleware for avatar upload
 const upload = multer({ storage: storage });
-
+*/
 module.exports = upload;

6a8b8069c4ea3226479e493ac3228e9632502b1b
Germain Bataille <g_bataille@hotmail.com>
2025-01-05 22:29:28 -0500

    vercel.json modified

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/api/comments', require('./routes/commentRoutes'));
 // Serve avatar images
 app.use('/uploads/avatars', express.static(path.join(__dirname, 'uploads/avatars')));
 
-const PORT = process.env.PORT || 5555;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);

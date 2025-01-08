857f3bea17f7f38a9dcdc86f31cd5b248f344a38
gbataille <g_bataille@hotmail.com>
2025-01-07 19:40:11 -0500

    Allow control origin 2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,11 @@ const upload = require('../middleware/uploadMiddleware');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
+router.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+   });
 router.post('/register', upload.single('avatar'), registerUser);
 router.post('/login', loginUser);
 router.get('/profile', authMiddleware, getMe);
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -4,6 +4,11 @@ const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
+router.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+   });
 router.get('/:threadId', getComments);
 router.post('/', authMiddleware, addComment);
 

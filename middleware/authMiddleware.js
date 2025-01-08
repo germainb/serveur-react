2ffdb1fd5dc7e3124c62913db59719c4c0e9f0d4
gbataille <g_bataille@hotmail.com>
2025-01-07 23:45:28 -0500

    put back token7

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1];
-   
+    //const token = req.headers.authorization.split(' ')[1];
+
+    let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    console.log('Found "Authorization" header');
+    // Read the ID Token from the Authorization header.
+    token = req.headers.authorization.split('Bearer ')[1];
+  } else if(req.cookies) {
+    console.log('Found "__session" cookie');
+    // Read the ID Token from cookie.
+    token = req.cookies.__session;
+  }
+
     if (!token) {
         return res.status(401).json({ message: 'No token, authorization denied' });
     }

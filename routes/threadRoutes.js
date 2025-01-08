ba346704002028cc46475d255ac4a05b27ed7c6a
gbataille <g_bataille@hotmail.com>
2025-01-07 19:36:59 -0500

    Allow control origin

--- routes/threadRoutes.js
+++ routes/threadRoutes.js
@@ -13,7 +13,11 @@ const {
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
-
+router.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+   });
 router.get('/', getThreads);
 router.get('/:id', authMiddleware, getThreadById);
 router.post('/', authMiddleware, createThread);

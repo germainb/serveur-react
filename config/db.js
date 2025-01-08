b21948b79c613582dd6369cead05ceefec6c4c49
gbataille <g_bataille@hotmail.com>
2025-01-08 15:58:12 -0500

    Revert "Mongo URL"
    This reverts commit d6aad638534bb0be29c1ad2705aa4e8f93956d7b.

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect("mongodb+srv://germainbataille:Quebec101!@cluster0.xbldt.mongodb.net/threadDB", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });

bd1689bc14c3a513e73f9c3187f21f1da4b4cf0f
gbataille <g_bataille@hotmail.com>
2025-01-15 17:52:06 -0500

    Commentaire25

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -24,7 +24,7 @@ const addComment = async (req, res) => {
 const getComments = async (req, res) => {
     try {
         const comments = await Comment.aggregate([
-            { $match: {thread: `${req.params.threadId}`}},
+            { $match: {thread: req.params.threadId}},
             { $lookup: { from: 'users', localField: 'author', foreignField: '_id', as: 'user' } }
     ])
         res.json(comments);

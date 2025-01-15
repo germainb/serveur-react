d1bd7c73b643750b597000d81d2cc250ac616a9f
gbataille <g_bataille@hotmail.com>
2025-01-15 12:23:47 -0500

    Commentaire4

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -6,6 +6,7 @@ const addComment = async (req, res) => {
         const threadId = req.params.threadId;
         const userId = req.params.userId;
         const { commentaire } = req.body;
+        console.log("Request body:"+req.body);
 
         const comment = await Comment.create({
             thread: threadId,

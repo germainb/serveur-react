f89794ea16beda771b7709250529684b166da787
gbataille <g_bataille@hotmail.com>
2025-01-16 13:01:22 -0500

    SendMail

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,5 +1,5 @@
 const Comment = require('../models/Comment');
-const User = require('../models/User');
+var nodemailer = require('nodemailer');
 
 // Add a comment to a thread
 const addComment = async (req, res) => {
@@ -7,8 +7,7 @@ const addComment = async (req, res) => {
         const threadId = req.params.threadId;
         const userId = req.params.userId;
         const { commentaire } = req.body;
-        console.log("Commentaire: "+commentaire);
-
+        
         const comment = await Comment.create({
             thread: threadId,
             author: userId,
@@ -26,10 +25,41 @@ const getComments = async (req, res) => {
     try {
         const comments = await Comment.find({thread: req.params.threadId}).populate('author');
         res.json(comments);
-        console.log("Request:" + req.params.threadId + "Response:" +res);
+        
     } catch (err) {
         res.status(500).json({ message: 'Server error'+err + "Request:" + req + "Response:" + res});
     }
 };
 
-module.exports = { addComment, getComments };
+// Add a comment to a thread
+const sendEmail = async (req, res) => {
+    try {
+        const { commentaire, email, nom } = req.body;
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: process.env.EMAIL,
+              pass: process.env.EMAIL_PASSWORD
+            }
+          });
+          
+          var mailOptions = {
+            from: process.env.EMAIL,
+            to:  email,
+            subject: 'Nouveau commentaire sur Face de bouc',
+            text: nom + " a ajouté un commentaire à votre message:<br>" + commentaire + "<br>Vous pouvez répondre à cette adresse:<br>https://client-react-ivory.vercel.app/"
+          };
+          
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
+       
+    } catch (err) {
+        res.status(500).json({ message: 'Server error:'+err });
+    }
+};
+module.exports = { addComment, getComments, sendEmail };
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var cors = require('cors');
-const { addComment, getComments } = require('../controllers/commentController');
+const { addComment, getComments, sendEmail } = require('../controllers/commentController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.use(cors())
 router.get('/:threadId', getComments);
 router.post('/:threadId/:userId', authMiddleware, addComment);
+router.post('/sendEmail', authMiddleware, sendEmail);
 
 module.exports = router;
